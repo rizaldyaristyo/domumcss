1064e312bcfd73996876e65076956df119827ef3
ARSTCreations <anandarizaldyaristyo@gmail.com>
2022-08-18 14:24:59 +0700

    Added collapsible

--- domum.js
+++ domum.js
@@ -54,3 +54,17 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
   buttons[idx].classList.add("carousel-button-s");
   autoSlide();
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("collactive");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
